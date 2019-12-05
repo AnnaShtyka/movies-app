84585c3023ffb4c320ae2169bee0f3c68df87c72
Anna <45462671+anna2990@users.noreply.github.com>
2019-12-05 14:46:57 +0300

    Update Nav.jsx

--- сomponents/Nav/Nav.jsx
+++ сomponents/Nav/Nav.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import classes from './Nav.module.css';
+import React from "react";
+import classes from "./Nav.module.css";
 
 const Nav = () => {
-    return (
-        <div>
-            <div className={classes.navbar}>
-                <div className="container">
-                    <div className={classes.info}>
-                        <h1>Movies Info</h1>
-                    </div>
-                    
-                </div>
-                <div className={classes.home}>
-                    <a href="/">Home</a>
-                </div>
-                <ul>
-                    <li>
-                      <i className="fab fa-imdb fa-5x" />
-                    </li>
-                </ul>
-            </div>
+  return (
+    <div>
+      <div className={classes.navbar}>
+        <div className="container">
+          <div className={classes.info}>
+            <h1>Movies Info</h1>
+          </div>
         </div>
-    )
-}
+        <div className={classes.home}>
+          <a href="/">
+            <h2>Home</h2>
+          </a>
+        </div>
+        <ul>
+          <li>
+            <i className="fab fa-imdb fa-5x" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
