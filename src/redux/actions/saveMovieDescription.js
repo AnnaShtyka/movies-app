bae543893623495ad7fd911a89cb7ab26ffcef54
Anna <45462671+anna2990@users.noreply.github.com>
2019-12-12 13:09:30 +0300

    add axios

--- src/redux/actions/saveMovieDescription.js
+++ src/redux/actions/saveMovieDescription.js
@@ -1,6 +1,11 @@
-export const saveMovieDescription = () => {
-  return {
-    type: "SAVE_MOVIE_DESCRIPTION",
-    payload: movie
-  };
+import axios from "axios";
+
+export const saveMovieDescription = id => dispatch => {
+  axios.get(`https://www.omdbapi.com/?apikey=5cb241a4&s=${id}`).then(response =>
+    dispatch({
+      type: "SAVE_MOVIE_DESCRIPTION",
+      payload: response.data
+    })
+  )
+  .catch(err => console.log(err));
 };
