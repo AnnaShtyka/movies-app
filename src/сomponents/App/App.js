5c740edc4b3045506d9eb1ab9e6820c543e149d7
Anna <45462671+anna2990@users.noreply.github.com>
2019-12-12 14:51:05 +0300

    add: mapStateToProps, functions

--- src/сomponents/App/App.js
+++ src/сomponents/App/App.js
@@ -1,42 +1,45 @@
-import React from "react";
-import { Provider } from "react-redux";
-import "./App.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import classes from "./Search.module.css";
 
-import Nav from "../Nav/Nav";
-import Films from "../Movies/MoviesCard";
-import Search from "../Search/Search";
-import FilmDescription from "../Description/FilmDescription";
-import FavoriteMovies from "../FavoriteMovies/FavoriteMovies";
+import { connect } from "react-redux";
 
-import store from "../../store";
+import { searchMovies } from "../../redux/actions/searchMovies";
+import { saveMovies } from "../../redux/actions/saveMovies";
 
-const App = () => {
-  return (
-    <Provider store={configureStore}>
-      <Router>
-        <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/">
-              <Search />
-              <Films />
-            </Route>
-            <Route
-              exact
-              path="/FilmDescription/"
-              component={FilmDescription}
-            ></Route>
-            <Route
-              exact
-              path="/FavoriteMovies/"
-              component={FavoriteMovies}
-            ></Route>
-          </Switch>
+class Search extends Component {
+  onChange = e => {
+    this.props.searchMovies(e.target.value);
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.saveMovies(this.props.text);
+  };
+  render() {
+    return (
+      <div className={classes.search}>
+        <div className={classes.search_container}>
+          <h1>
+            <i className="fa fa-search" /> Search for a movie ..
+          </h1>
+          <form className={classes.searchForm} onSubmit={this.onSubmit}>
+            <input
+              type="text"
+              name="searchText"
+              placeholder="Search Movies ..."
+              onChange={this.onChange}
+            />
+            <button type="submit">Search</button>
+          </form>
         </div>
-      </Router>
-    </Provider>
-  );
-};
+      </div>
+    );
+  }
+}
 
-export default App;
+const mapStateToProps = state => ({
+  text: state.movies.text
+});
+
+export default connect(mapStateToProps, { searchMovies, saveMovies })(Search);
+
