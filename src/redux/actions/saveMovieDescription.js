4e2b975a9b416bb243d3a81b8dd45eaa8d243ab6
Anna <45462671+anna2990@users.noreply.github.com>
2019-12-13 16:24:38 +0300

    Update saveMovieDescription.js

--- src/redux/actions/saveMovieDescription.js
+++ src/redux/actions/saveMovieDescription.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-export const saveMovieDescription = id => dispatch => {
-  axios.get(`https://www.omdbapi.com/?apikey=5cb241a4&s=${id}`).then(response =>
-    dispatch({
-      type: "SAVE_MOVIE_DESCRIPTION",
-      payload: response.data
-    })
-  )
-  .catch(err => console.log(err));
+export const saveMovieDescription = data => ({
+  type: "SAVE_MOVIE_DESCRIPTION",
+  payload: data
+});
+export const fetchMovieDescription = id => dispatch => {
+  axios
+    .get(`https://www.omdbapi.com/?apikey=5cb241a4&s=${id}`)
+    .then(response => dispatch(saveMovieDescription(response.data)))
+    .catch(err => console.log(err));
 };
