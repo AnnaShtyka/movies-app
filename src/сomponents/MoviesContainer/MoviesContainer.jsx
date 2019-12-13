c0af0f387c3a7d7d876fe891d5fd813817d98fa2
Anna <45462671+anna2990@users.noreply.github.com>
2019-12-13 16:28:13 +0300

    initial commit

---
+++ src/сomponents/MoviesContainer/MoviesContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { connect } from "react-redux";
+
+import MovieCard from "../Movies/MovieCard";
+import classes from './MoviesContainer.module.css';
+
+export const MoviesContainer = props => {
+  const { movies } = props;
+  return (
+    <div className={classes.container}>
+      {!movies || !movies.Search
+        ? null
+        : movies.Search.map((movie, i) => <MovieCard key={i} {...movie} />)}
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  movies: state.saveMovies.movies
+});
+
+export default connect(mapStateToProps)(MoviesContainer);
