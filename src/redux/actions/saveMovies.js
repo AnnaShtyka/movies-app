26411f168332d680d20169622ee9d16bcc2c17af
Anna <45462671+anna2990@users.noreply.github.com>
2019-12-13 16:21:28 +0300

    fixed action

--- src/redux/actions/saveMovies.js
+++ src/redux/actions/saveMovies.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-export const saveMovies = text => dispatch => {
-  axios
-    .get(`https://www.omdbapi.com/?apikey=5cb241a4&s=${text}`)
-    .then(response =>
-      dispatch({
-        type: "SAVE_MOVIES",
-        payload: response.data
-      })
-    )
-    .catch(err => console.log(err));
+export const saveMovies = (data) => ({
+  type: "SAVE_MOVIES",
+  payload: data
+})
+export const fetchMovies = text => dispatch => {
+axios
+.get(`https://www.omdbapi.com/?apikey=5cb241a4&s=${text}`)
+.then(response =>
+dispatch(saveMovies(response.data))
+)
+.catch(err => console.log(err));
 };
