70e2ec0898a46f605cb2d298df9e5cb206a562c1
Anna <45462671+anna2990@users.noreply.github.com>
2019-12-16 20:45:02 +0200

    Update and rename saveMovieDescription.js to movie.js

---
+++ src/redux/reducers/movie.js
@@ -0,0 +1,19 @@
+import {GET_MOVIE_DESCRIPTION} from '../actions/types';
+
+const initialState = {
+  movie: []
+};
+
+const movie = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_MOVIE_DESCRIPTION:
+      return {
+        ...state,
+        movie: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default movie;
